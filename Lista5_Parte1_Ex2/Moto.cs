22c74fb637643867d33d0c40c38853d81c8aa4ff
Bianca Ribeiro <132418587+biancafdribeiro@users.noreply.github.com>
2023-10-23 22:35:31 -0300

    Add files via upload

---
+++ Lista5_Parte1_Ex2/Moto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Crie uma classe abstrata Veiculo com um método abstrato Mover(). Crie duas classes
+derivadas de Veiculo: Carro e Moto. Implemente o método Mover() em ambas as
+classes para exibir mensagens indicando o movimento do veículo.*/
+
+namespace Lista5_Parte1_Ex2
+{
+    public class Moto : Veiculo
+    {
+        public override void Mover()
+        {
+            Console.WriteLine("Vrooom...");
+        }
+    }
+}
---
+++ Lista5_Parte1_Ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Crie uma classe abstrata Veiculo com um método abstrato Mover(). Crie duas classes
+derivadas de Veiculo: Carro e Moto. Implemente o método Mover() em ambas as
+classes para exibir mensagens indicando o movimento do veículo.*/
+
+namespace Lista5_Parte1_Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+            carro.Mover();
+
+            Moto moto = new Moto();
+            moto.Mover();
+
+            Console.ReadKey();
+        }
+    }
+}
