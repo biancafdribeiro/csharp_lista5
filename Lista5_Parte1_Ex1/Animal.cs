cac3b99480dca47ffbb6aa9acbe4bf8385f209ca
Bianca Ribeiro <132418587+biancafdribeiro@users.noreply.github.com>
2023-10-23 22:17:19 -0300

    Add files via upload

---
+++ Lista5_Parte1_Ex1/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Crie uma classe abstrata chamada Animal com um método abstrato EmitirSom(). Crie
+três classes derivadas de Animal: Cachorro, Gato e Vaca. Implemente o método
+EmitirSom() em cada classe para que cada animal emita um som diferente.*/
+
+namespace Lista5_Parte1_Ex1
+{
+    public abstract class Animal
+    {
+        public abstract void EmitirSom();
+    }
+}
